86754a62e145b2ba61c6768947f905c7f6bc970e
Maciej Białowolski <maciekbestia@gmail.com>
2025-06-03 09:31:59 +0200

    Add arithmetic_operations

---
+++ arithmetic_operations/arithmetic_operations.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int x=5;
+int y=2;
+int main()
+{
+    int sum = x+y;
+    int difference = x-y;
+    int multiply = x*y;
+    int divide = x/y;
+    int mod = x%y;
+    cout<<sum<<endl;
+    cout<<difference<<endl;
+    cout<<multiply<<endl;
+    cout<<divide<<endl;
+    cout<<mod<<endl;
+    return 0;
+}
